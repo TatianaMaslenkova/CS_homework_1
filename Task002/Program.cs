e1963c07f4a96a757edc26e590cd6f3f640a3fbc
Tatiana Maslenkova <tsevastianova21@gmail.com>
2022-09-20 12:36:58 +0300

    Task 002

---
+++ Task002/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.Write("Введите число 1: ");
+int number1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число 2: ");
+int number2 = int.Parse(Console.ReadLine()!);
+
+if(number1 == number2)
+{
+    Console.WriteLine("Числа равны");
+}
+else
+{
+    int min = Math.Min(number1, number2);
+    Console.WriteLine($"Min = {min}");
+
+    int max = Math.Max(number1, number2);
+    Console.WriteLine($"Max = {max}");
+}
