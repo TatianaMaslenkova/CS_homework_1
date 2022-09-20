9c0f8086dad5a92b431a8ac90dcd6fb15fcc412c
Tatiana Maslenkova <tsevastianova21@gmail.com>
2022-09-20 15:56:28 +0300

    Task 006

---
+++ Task006/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int number = Math.Abs(int.Parse(Console.ReadLine()));
+
+if(number % 2 == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число является нечетным");
+}
