fe2807a8005baa6a818dc37db3bbd88a2e9c4b1d
Tatiana Maslenkova <tsevastianova21@gmail.com>
2022-09-20 16:38:46 +0300

    Task 008

---
+++ Task008/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите любое положительное число больше 1: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int count = 2;
+
+if(number <= 1)
+    {
+        Console.WriteLine("Введенное число не соответствует условию");
+    }
+else
+{
+    while(count <= number)
+    {
+        Console.Write($"{count}, ");
+        count+=2;
+    }
+}
